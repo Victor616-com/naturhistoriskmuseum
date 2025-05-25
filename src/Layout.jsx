ec8078d080a1738c359cf874aa8393075e17d81e
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-05-26 00:26:16 +0200

    fixed scrolling to top on different path

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./views/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 
 function Layout() {
     return (
         <>
+            <ScrollToTop />
             <NavBar />
             <div style={{ marginTop: 'var(--nav-height, 60px)' }}>
                 <Outlet />
