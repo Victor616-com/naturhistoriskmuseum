11e32ed30dfb640657de1d588f8f51783ee12f60
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-05-26 00:13:32 +0200

    changed other images and some text

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -12,13 +12,13 @@ const Topics = () => {
                     />
                 <TopicCard 
                         imageUrl="/naturhistoriskmuseum/assets/images/topic2.jpg"
-                        title= "Science, nature & meaning"
-                        description= "Where do we find beauty, responsibility, and wonder in the world around us?"
+                        title= "Poaching & extinction"
+                        description= "What do we lose when a species disappears—and why does it matter?"
                     />
                 <TopicCard 
                         imageUrl="/naturhistoriskmuseum/assets/images/topic3.jpg"
-                        title= "Science, nature & meaning"
-                        description= "Where do we find beauty, responsibility, and wonder in the world around us?"
+                        title= "Denmark’s changing landscape"
+                        description= "Climate shifts, land use, and how we stay connected to nature."
                     />
             </div>
             
